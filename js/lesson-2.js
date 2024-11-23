0668b6471a3012b757a637731442fd0b779515cb
Vladyslav Rzaiev <vlad.rzaev@gmail.com>
2024-11-23 09:44:24 +0100

    add task1

---
+++ js/lesson-2.js
@@ -0,0 +1,29 @@
+// * Масиви: літерал масиву, елементи, індексація, довжина
+//  Створіть масив styles з елементами «Джаз» та «Блюз».
+// Додайте "Рок-н-рол" в кінець.
+// Замініть значення "Блюз" на "Класика".
+// Видалить перший елемент масиву і виведіть його в консоль.
+// Вставте «Реп» і «Реггі» на початок масиву.
+
+// const styles = ['Джаз', 'Блюз'];
+
+// styles.push('Рок-н-рол');
+// // styles.unshift('Класика');
+
+// // const style = ['Класика'];
+// // const newStyles = style.concat(styles);
+// styles.unshift('Rep');
+// // styles[1] = 'Класика';
+// const index = styles.indexOf('Блюз');
+// if (index !== -1) {
+//   styles[index] = 'Класика';
+// }
+// // for (let i = 0; i < styles.length; i++) {
+// //   console.log(`${i + 1}: ${styles[i]}`);
+// // }
+
+// for (const style of styles) {
+//   console.log(style);
+// }
+
+// console.log(styles);
