947a88a9590b9c0fc9787824e977334f14b92f59
Oleksandr Kostiuchenko <aleksandrkostucenko11@gmail.com>
2025-01-11 10:53:15 +0100

    add-theme-switcher

--- js/lesson-5.js
+++ js/lesson-5.js
@@ -4,9 +4,44 @@
 
 // console.log(localStorage);
 
+//TODO: Theme switcher 
+//* Imports 
 import {
   addDataToLocalStorage,
   getDataFromLocalStorage,
   removeDataFromLocalStorage,
   clearDataFromLocalStorage,
 } from './local-storage-api.js';
+
+import { LS_KEY } from './ls-Key.js';
+
+//* Refs
+const body = document.querySelector('body');
+const checkboxEl = document.querySelector('#theme-switch');
+
+//* Callback 1
+const renderPage = () => {
+  const LSdata = getDataFromLocalStorage(LS_KEY);
+  if (LSdata === 'dark') {
+    body.classList.replace('light', 'dark');
+    checkboxEl.checked = true;
+  } else {
+    body.classList.replace('dark', 'light');
+  }
+}
+
+//* Callback 2
+const onCheckboxClick = event => {
+  console.log(checkboxEl.checked);
+  if (checkboxEl.checked) {
+    body.classList.replace('light', 'dark');
+    addDataToLocalStorage(LS_KEY, 'dark');
+  } else {
+    body.classList.replace('dark', 'light');
+    addDataToLocalStorage(LS_KEY, 'light');
+  }
+}
+
+//* Add event listeners 
+checkboxEl.addEventListener('click', onCheckboxClick);
+document.addEventListener('DOMContentLoaded', renderPage);
